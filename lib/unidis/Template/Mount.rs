a8c8482a31f13e6d35fcdb5f88f671b844723734
Edmund Wu <fangkazuto@gmail.com>
2021-08-04 01:26:43 -0400

    lib/unidis: remount procfs instance

--- lib/unidis/Template/Mount.rs
+++ lib/unidis/Template/Mount.rs
@@ -3,7 +3,8 @@ use std::ptr;
 use libc::*;
 
 /* Mount is a simple structure adapted for the mount() syscall. */
-pub struct Mount<'a> {
+pub struct Mount<'a>
+{
     pub source: &'a str,
     pub target: &'a str,
     pub fstype: &'a str,
@@ -13,10 +14,21 @@ pub struct Mount<'a> {
 
 /* MOUNTPOINTS is the mountings to perform for a functioning system
 within the user namespace.  Paths are relative to the new "/". */
-pub const MOUNTPOINTS: [Mount; 1] = [
-  // Rebind /dev in a user namespace if /dev is not readable (unprivileged)
-  Mount {
-    source: "/dev", target: "dev/", fstype: "",
-    mountflags: MS_REC|MS_BIND|MS_NOSUID, data: ptr::null(),
-  },
+pub const MOUNTPOINTS: [Mount; 2] = [
+    // Remount /proc if CLONE_NEWPID was called
+    Mount {
+        source: "none",
+        target: "proc/",
+        fstype: "proc",
+        mountflags: MS_NOSUID | MS_NODEV | MS_NOEXEC,
+        data: ptr::null(),
+    },
+    // Rebind /dev in a user namespace if /dev is not readable (unprivileged)
+    Mount {
+        source: "/dev",
+        target: "dev/",
+        fstype: "",
+        mountflags: MS_REC | MS_BIND | MS_NOSUID,
+        data: ptr::null(),
+    },
 ];
